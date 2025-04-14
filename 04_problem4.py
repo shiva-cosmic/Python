2d61e97971a27fb9d8361b5114e701d9fa158a71
Shivanshh369 <shivanshhh369@gmail.com>
2025-04-15 00:47:40 +0530

    Practice sets

---
+++ 04_problem4.py
@@ -0,0 +1,6 @@
+a = int (input("ENTER NUMBER 1:"))
+b = int (input("ENTER NUMBER 2:"))
+
+
+print("a is greater than b is",a>b)
+40
---
+++ 06_problem6.py
@@ -0,0 +1,6 @@
+a = int(input("Enter your number:"))
+
+print("The square of the number is",a**2)
+
+
+#print("the square of the number is",a^2) #Incorrect for finding square of a number in Python
