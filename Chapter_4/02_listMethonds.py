6ae9e6b4e39a6d79e213f1462a0d92856bf51e78
Shivanshh369 <shivanshhh369@gmail.com>
2025-05-16 20:16:25 +0530

    listMethod

---
+++ Chapter_4/02_listMethonds.py
@@ -0,0 +1,15 @@
+friends = ["Apple", "Orange", 5, 345.06, False,"Aakash","Rohan"]
+print(friends)
+
+friends.append("Harry")
+
+print(friends)
+
+#Sort
+l1 =[1,34,62,2,6,11]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3, 33333) #Insert 33333 such that its index in the list is 3
+# l1.pop(3)
+l1.remove(2)
+print(l1)
