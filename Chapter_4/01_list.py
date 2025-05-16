7700cb99e86d392f02721946af9ce09af839e383
Shivanshh369 <shivanshhh369@gmail.com>
2025-05-16 19:21:01 +0530

    List

---
+++ Chapter_4/01_list.py
@@ -0,0 +1,10 @@
+friends = ["Apple", "Orange", 5, 345.06, False,"Aakash","Rohan"]
+
+#List are Mutable, String are not mutable.(List mein changes kr skte hai, string mein nhi kr skte)
+print(friends[0])
+friends[0] = "Grapes"
+print(friends[0])
+
+
+#Sclicing 
+print(friends[1:4]) 
