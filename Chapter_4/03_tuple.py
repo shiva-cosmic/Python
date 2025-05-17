5d79954490cd12a606bf2f59d19e96db6a78c54c
Shivanshh369 <shivanshhh369@gmail.com>
2025-05-17 23:16:57 +0530

    List&TupleUpdate

---
+++ Chapter_4/03_tuple.py
@@ -0,0 +1,3 @@
+a = (1,2,5,6, True, 23.3 ,"shivam")
+print(type(a))
+print(a)
---
+++ Chapter_4/04_tupleMethod.py
@@ -0,0 +1,11 @@
+a = (1,2,5,6, True, 23.3 ,"shivam")
+
+print(a)
+
+no = a.count(23.3)
+print(no)
+
+i = a.index(23.3)
+print(i)
+
+print(len(a))
