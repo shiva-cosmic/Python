cdeea05ae0cbcd39c36849f4d0cdde5e48af903a
Shivanshh369 <shivanshhh369@gmail.com>
2025-05-18 20:01:39 +0530

    Problem2Completed

---
+++ Chapter_4/Ch-4_PracticeSet/02_Problem2.py
@@ -0,0 +1,20 @@
+# 2. Write a program to accept marks of 6 students and display them in a sorted manner.
+
+marks =[]
+
+m1 =int(input("Enter marks of 1st student:"))
+marks.append(m1)
+m2 =int(input("Enter marks of 2st student:"))
+marks.append(m2)
+m3 =int(input("Enter marks of 3st student:"))
+marks.append(m3)
+m4 =int(input("Enter marks of 4st student:"))
+marks.append(m4)
+m5 =int(input("Enter marks of 5st student:"))
+marks.append(m5)
+m6 =int(input("Enter marks of 6st student:"))
+marks.append(m6)
+
+marks.sort()
+
+print(marks)
