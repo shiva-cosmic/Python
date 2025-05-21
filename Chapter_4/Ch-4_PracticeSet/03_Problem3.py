99a9ea4ef63ff45a3ffb3f95c8fd9dfa9d2ee7d5
Shivanshh369 <shivanshhh369@gmail.com>
2025-05-21 19:52:31 +0530

    Problem 3 to 5

---
+++ Chapter_4/Ch-4_PracticeSet/03_Problem3.py
@@ -0,0 +1,5 @@
+#Check that tuple type cannot be changed in python.
+
+a =(34, 234, "Harry")
+
+a[2] = "Larry" #tuple is immutable you cannot change it once it aggined
---
+++ Chapter_4/Ch-4_PracticeSet/04_Problem4.py
@@ -0,0 +1,5 @@
+#Write a program to sum a list with 4 numbers
+
+l =[34, 23, 645, 123]
+
+print(sum(l))
