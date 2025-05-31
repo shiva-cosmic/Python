f13b51e4b41e4d5bf948b7066b65bf46cab42ff5
Shivanshh369 <shivanshhh369@gmail.com>
2025-05-31 21:58:52 +0530

    Ch5 Complete

---
+++ Chapter_5/05_setUnionIntersec.py
@@ -0,0 +1,8 @@
+s1 ={1,45,6}
+s2 ={7,8,1,78}
+
+print(s1.union(s2))
+#This print all the  values in both sets s1 s2 in sorted form, And Yeah Non repeatable elem. rules also apply(set rule)
+
+print(s1.intersection(s2))
+#It print common values in both the sets s1 s2.
