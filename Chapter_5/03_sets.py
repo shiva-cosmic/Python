aa83eb0beb6f2d79dc8629e405982a5b7a735663
Shivanshh369 <shivanshhh369@gmail.com>
2025-05-31 20:00:16 +0530

    One Change

--- Chapter_5/03_sets.py
+++ Chapter_5/03_sets.py
@@ -2,4 +2,4 @@ e = set() #Empty set
 #Dont use s ={} as a empty set, as it will create an empty dictionary
 print(type(e))
 
-s = {1,5,32,54,5,5,5}
+s = {1,5,32,54,5,5,5} # 5 will consider at onces only cause set never count repeated values
