78ac2fedab669fd71cf69256e2224dffd613702f
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-01 22:37:05 +0530

    Ladder

---
+++ Chapter_6/02_ifelifelseLadder.py
@@ -0,0 +1,25 @@
+a = int(input("Enter your age: "))
+
+# if elif else ladder
+if(a>=18):
+  print("You are above the age of consent")
+  print("Good for you")
+elif(a<0):
+  print("You are entering an invalid negative age")
+elif(a==0):
+  print("You are entering 0 which is not a valid age")
+else:
+  print("You are below the age of consent")
+
+print("End of Program")
+
+
+# QUIZ
+# Write a program to print yes when the age entered by the user is greater than or equal to 18.
+
+b = int(input("Enter your age:"))
+
+if(b>=18):
+  print("Yes")
+else:
+  print("No")  
