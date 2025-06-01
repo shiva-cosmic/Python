a77f1b806c9ac3c3891d0dde796c572e7f09eaab
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-01 22:17:29 +0530

    Changes

--- Chapter_6/01_Conditionals.py
+++ Chapter_6/01_Conditionals.py
@@ -1,7 +1,14 @@
 a = int(input("Enter your age: "))
 
-if(a>18):
-  print("You are above the age of concent")
+# if elif else ladder
+if(a>=18):
+  print("You are above the age of consent")
   print("Good for you")
+elif(a<0):
+  print("You are entering an invalid negative age")
+elif(a==0):
+  print("You are entering 0 which is not a valid age")
 else:
-  print("You are below the age of concent")
+  print("You are below the age of consent")
+
+print("End of Program")
