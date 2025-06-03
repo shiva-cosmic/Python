0537049e39b29abd3c15ebcc4c9cce185a69de80
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-03 23:17:49 +0530

    Pass Statement

---
+++ Chapter_7/08_pass.py
@@ -0,0 +1,7 @@
+for i in range(645):
+    pass #it is a null stament in python, it instruct "Do nothing" that means it will not give error and further code will run, and wwhenever you want to complete this for loop you will come and complete it.
+
+i =0
+while(i<45):
+  print(i)
+  i+=1
