af0cc1a6f1d3e4fcc3dc9353a93832f171b3ea54
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-03 22:43:52 +0530

    LoopWithElse

---
+++ Chapter_7/06_forLoopWithElse.py
@@ -0,0 +1,9 @@
+l = [1,7,8]
+for item in l:
+  print(item)
+else:
+  print("done") 
+  # this is printed when the loop exhausts!
+
+
+  # Practical use case is less but interviwer will ask to entangled you.(Uljhane k liye puch skte hai)
