22a25ec43888e4f87d890d2449cbb9730092aaa8
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-03 22:11:19 +0530

    Forloop

---
+++ Chapter_7/04_forLoop.py
@@ -0,0 +1,12 @@
+# This itself consider the intial value 0 if we dont give it, so the output is 0 to 3. 
+for i in  range (4):
+  print(i)
+
+
+# Step_Size range(start, stop, step_size)
+
+for i in range (0,50,4):
+       print(i)
+
+
+
