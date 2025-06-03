de152aa14c7589ef007b368e8e67a74070fb114f
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-03 22:17:39 +0530

    loopWithListTupleString

---
+++ Chapter_7/05_forLoopIteratable.py
@@ -0,0 +1,19 @@
+# for loop is used to iterate through a sequence like list, tuple or string [iterables]
+
+# For List
+list = [1,4,3,4,213,3,133]
+
+for i in list:
+  print(i)
+
+# For Tuple
+tuple =(5,231,56,221)
+
+for i in tuple:
+  print(i)
+
+
+# For String
+String = "Shivansh"
+for i in String:
+  print(i)
