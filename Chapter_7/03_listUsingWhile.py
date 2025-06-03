0f127313c355179ae748807b986ee9d3b737aaf6
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-03 21:48:10 +0530

    Loop on list

---
+++ Chapter_7/03_listUsingWhile.py
@@ -0,0 +1,11 @@
+l = [1, "Shivansh", False, "This", "Rohan", "Shubham", "Shubhi"]
+
+
+i = 0
+
+while (i<len(l)):
+  print(l[i])
+  i +=1
+
+
+  # So there is a list we have to print, we does this by applying loop on it, we intialize the loop with i = 0, then we give the condition i<len(), this len(l) means it count the toatal length of  list >(l), according to that it runs the till the final count,In this case we have  7 elements in list, so that itrate till 7 and then it will stop.
