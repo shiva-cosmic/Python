b9dd88b2f95979991827e203b6907d07c143f9fc
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-06 23:26:40 +0530

    ps

--- Chapter_7/02_WhileLoop.py
+++ Chapter_7/02_WhileLoop.py
@@ -25,30 +25,3 @@ i =0
 while (i < 51):
   print(i)
   i +=1
-i = 1
-
-while(i<6): 
-  print(i)
-  i +=1
-#This block keeps executing until the condition is true
-
-  '''
-
-  Output:
-  1
-  2
-  3
-  4
-  5
-
-  '''
-
-
-  # Quick Quiz
-
-  # Write a program to print 1 to 50 using a while loop
-
-i =0
-while (i < 51):
-  print(i)
-  i +=1
