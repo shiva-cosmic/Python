7e7cc604c3f5880b0feb50fc6edf7f2037112623
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-09 18:32:00 +0530

    Update

---
+++ Chapter_7/Ch-7_PracticeSet/09_Problem9.py
@@ -0,0 +1,6 @@
+# Write a program to print the following star pattern:
+'''
+***
+* *
+***   for n=3
+'''
---
+++ Chapter_7/Ch-7_PracticeSet/10_Problem10.py
@@ -0,0 +1,1 @@
+# Write a program to print multiplication table of n using for loops in reversed order.
