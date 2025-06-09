d9b4547f1a4732562c958796539e65ad6aa133fc
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-09 18:25:07 +0530

    Update

--- Chapter_7/Ch-7_PracticeSet/07_Problem7.py
+++ Chapter_7/Ch-7_PracticeSet/07_Problem7.py
@@ -5,6 +5,7 @@
 
 n = int(input("Enter the number: "))
 for i in range(1, n+1):
-    print(" "* (n-1), end = "")
+    print(" "* (n-i), end = "")
     print("*"* (2*i-1), end = "")
-    print("\n")
+    print("")
+  
