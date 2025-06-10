8a4c1ad782a0d7cc0720a368940eec431f0f5b7b
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-10 18:37:32 +0530

    Correction

--- Chapter_7/Ch-7_PracticeSet/10_Problem10.py
+++ Chapter_7/Ch-7_PracticeSet/10_Problem10.py
@@ -4,9 +4,3 @@ n = int(input("Enter the number: "))
 
 for i in range(1, 11):
   print(f"{n} X {11 - i} = { n *(11 -i )}")
-# Write a program to print multiplication table of n using for loops in reversed order.
-
-n = int(input("Enter the number: "))
-
-for i in range(1, 11):
-  print(f"{n} X {11 - i} = { n *(11 -i )}")
