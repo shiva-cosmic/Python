0690d2d7ca6edaf15b584558bf2db1d0316669a4
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-10 18:39:15 +0530

    date

--- Chapter_7/Ch-7_PracticeSet/08_Problem8.py
+++ Chapter_7/Ch-7_PracticeSet/08_Problem8.py
@@ -9,3 +9,14 @@ n = int (input("Enter the number: "))
 for i in range(1, n+1):
   print("*"*i, end="")
   print("")
+# Write a program to print the following star pattern:
+'''
+*
+**
+***   for n = 3
+'''
+
+n = int (input("Enter the number: "))
+for i in range(1, n+1):
+  print("*"*i, end="")
+  print("")
