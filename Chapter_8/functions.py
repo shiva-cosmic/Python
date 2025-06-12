ca7a7bda8b6b067f73056fe4a01f5c1c3d3a2fbe
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-12 20:25:22 +0530

    Function Complete

--- Chapter_8/functions.py
+++ Chapter_8/functions.py
@@ -1,19 +1,39 @@
-# Don't work like this, dont use this
-a = int(input("Enter your number: "))
-b = int(input("Enter your number: "))
-c = int(input("Enter your number: "))
+# # Don't work like this, dont use this
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
 
 
-average = (a + b + c)/3
+# average = (a + b + c)/3
 
-print(average)
-
-
-a = int(input("Enter your number: "))
-b = int(input("Enter your number: "))
-c = int(input("Enter your number: "))
+# print(average)
 
 
-average = (a + b + c)/3
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
 
-print(average)
+
+# average = (a + b + c)/3
+
+# print(average)
+
+
+
+# Function
+
+def avg():
+  a = int(input("Enter your number: "))
+  b = int(input("Enter your number: "))
+  c = int(input("Enter your number: "))
+
+
+  average = (a + b + c)/3
+
+  print(average)
+
+
+avg()
+avg()
+avg()
+avg() #ab ye 4 bar hoga without extra coding just write at onces in a fucntion then call that  fucntion where you want to apply  that fucntion. this improve quaility of code and reduce the mess
