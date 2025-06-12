10f9b96c92960be21b6f0d9c8cd9e0d48badd1ef
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-12 20:19:49 +0530

    Up

--- Chapter_8/functions.py
+++ Chapter_8/functions.py
@@ -1,6 +1,18 @@
-a =12
-b =45
-c =56
+# Don't work like this, dont use this
+a = int(input("Enter your number: "))
+b = int(input("Enter your number: "))
+c = int(input("Enter your number: "))
+
+
+average = (a + b + c)/3
+
+print(average)
+
+
+a = int(input("Enter your number: "))
+b = int(input("Enter your number: "))
+c = int(input("Enter your number: "))
+
 
 average = (a + b + c)/3
 
