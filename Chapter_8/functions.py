df8187a859ea1640d497167b2c70ff41c19dac09
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-14 22:47:11 +0530

    Update

--- Chapter_8/functions.py
+++ Chapter_8/functions.py
@@ -37,3 +37,42 @@ avg() #Function Call
 avg()
 avg()
 avg() #ab ye 4 bar hoga without extra coding just write at onces in a fucntion then call that  fucntion where you want to apply  that fucntion. this improve quaility of code and reduce the mess
+# # Don't work like this, dont use this
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+
+# average = (a + b + c)/3
+
+# print(average)
+
+
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+
+# average = (a + b + c)/3
+
+# print(average)
+
+
+
+# Function
+
+def avg():
+  a = int(input("Enter your number: "))
+  b = int(input("Enter your number: "))
+  c = int(input("Enter your number: "))
+
+
+  average = (a + b + c)/3
+
+  print(average)
+
+
+avg() #Function Call
+avg()
+avg()
+avg() #ab ye 4 bar hoga without extra coding just write at onces in a fucntion then call that  fucntion where you want to apply  that fucntion. this improve quaility of code and reduce the mess
