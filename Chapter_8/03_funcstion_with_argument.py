57cb9b27c01d4ba59967a2f8104d0ce6464ebcc1
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-15 19:24:23 +0530

    Normal way

---
+++ Chapter_8/03_funcstion_with_argument.py
@@ -0,0 +1,10 @@
+# Function with Argument
+
+def goodDay(name, ending):   #Yaha name ek parameter hai , ending dusra parameter hai jo print ending mein chala jayga.
+  print("Good Day, " + name)
+  print(ending)
+
+
+goodDay("Shivansh" , "Thank you")
+goodDay("Rohan" , "Thank you")
+goodDay("Divya" , "Thanks" )
