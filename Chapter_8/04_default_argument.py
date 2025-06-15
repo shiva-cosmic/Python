a5c89c7977b9fe44485e0e4a7e0f29717a09bc3d
Shivanshh369 <shivanshhh369@gmail.com>
2025-06-15 22:02:18 +0530

    Defult Argument

---
+++ Chapter_8/04_default_argument.py
@@ -0,0 +1,7 @@
+def goodDay(name, ending = "Thank you"): #ending = thanks you that menas this is default value if the paramenter argument is no0t passed than it will print otherwise given parameter passed
+    print(f"Good Day, {name} ")
+    print(ending)
+
+
+goodDay("Harry", "Thanks")    
+goodDay("Rohan") 
