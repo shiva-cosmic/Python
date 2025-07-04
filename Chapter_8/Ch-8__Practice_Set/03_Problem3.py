ef7064554f4eb291583e6f7f217732d788a6e5be
Shivanshh369 <shivanshhh369@gmail.com>
2025-07-04 19:06:26 +0530

    avoiding /n

---
+++ Chapter_8/Ch-8__Practice_Set/03_Problem3.py
@@ -0,0 +1,8 @@
+# how do you prevent a python print() function to print a new line at the end.
+
+
+print("a")
+print("b")
+# to avoid next line we use end with no space
+print("c", end="")
+print("d", end="")
