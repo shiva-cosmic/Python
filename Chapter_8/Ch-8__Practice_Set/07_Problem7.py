f2a6c2d90f702cf7b1ed47b5306f44515b6d1adf
Shivanshh369 <shivanshhh369@gmail.com>
2025-07-06 14:00:45 +0530

    Strip Problem7

--- Chapter_8/Ch-8__Practice_Set/07_Problem7.py
+++ Chapter_8/Ch-8__Practice_Set/07_Problem7.py
@@ -1,9 +1,11 @@
 # Write a python function to remove a given word from a list ad strip it at the same time.
 
 def rem(l, word):
+  n = []
   for item in l:
-    l.remove(word)
-    return l
+      if not(item == word):
+         n.append(item.strip(word))
+      return n
   
 l = ["Harry", "Rohan", "Shubham", "an"]
 
